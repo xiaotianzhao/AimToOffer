5671b361194dc1f59e4893d9c0cf76e6c6b2202a
Xiaotian Zhao <393504144@qq.com>
2018-09-02 16:09:43 +0800

    Update printListFromTailToHead.h

--- printListFromTailToHead.h
+++ printListFromTailToHead.h
@@ -1,20 +1,38 @@
+#ifndef PRINTLISTFROMTAILTOHEAD_H_INCLUDED
+#define PRINTLISTFROMTAILTOHEAD_H_INCLUDED
+
+#include <vector>
+#include <stack>
+
+#endif // PRINTLISTFROMTAILTOHEAD_H_INCLUDED
+
+using namespace std;
+
+struct ListNode {
+      int val;
+      struct ListNode *next;
+      ListNode(int x) :
+            val(x), next(NULL) {
+      }
+};
+
 class Solution {
 public:
     vector<int> printListFromTailToHead(ListNode* head) {
-        vector<int> numArray;
-		stack<int> numStack;
+        vector<int> num_array;
+        stack<int> num_stack;
 
-		ListNode* pointer = head;
+        ListNode* pointer = head;
+        while(pointer != NULL){
+            num_stack.push(pointer->val);
+            pointer = pointer->next;
+        }
 
-		while (pointer != NULL){
-			numStack.push(pointer->val);
-			pointer = pointer->next;
-		}
+        while(!num_stack.empty()){
+            num_array.push_back(num_stack.top());
+            num_stack.pop();
+        }
 
-		while (!numStack.empty()){
-			numArray.push_back(numStack.top());
-			numStack.pop();
-		}
-		return numArray;
+        return num_array;
     }
 };
